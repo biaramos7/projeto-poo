5a21db9d459172008a7e8a18dbddb064038d4a88
Amanda <82603073+mazziniamanda@users.noreply.github.com>
2022-05-02 14:36:18 -0300

    Update DepositoNegativoException.java
    Ajuste

--- SistemaBancarioProjetoFinal/src/br/com/serratec/excecao/DepositoNegativoException.java
+++ SistemaBancarioProjetoFinal/src/br/com/serratec/excecao/DepositoNegativoException.java
@@ -1,4 +1,8 @@
 package br.com.serratec.excecao;
 
 public class DepositoNegativoException extends Throwable {
+	
+	public DepositoNegativoException(String mensagemErro) {
+		super(mensagemErro);
+	}
 }
